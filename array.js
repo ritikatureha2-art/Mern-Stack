79b7e90a74c0d53436ffbf9f6baf5709299aa5e4
ritikatureha2-art <ritikatureha2@gmail.com>
2026-06-28 23:04:34 +0530

    Add files via upload

---
+++ array.js
@@ -0,0 +1,32 @@
+// push 
+ let arr=["noida","nainital","rishikesh"];
+ console.log(arr);
+ arr.push("shimlaa")
+ console.log(arr);
+
+ //unshift(val)
+arr.unshift("manali")
+console.log(arr);
+
+arr.splice(2,0,"lucknow","agra ")
+console.log(arr);
+
+arr.pop()
+console.log(arr);
+
+arr.shift()
+console.log(arr);
+
+arr.splice(2,2)
+console.log(arr);
+
+let names=["ritika","mansi","arya","tanya"];
+console.log(names);
+
+let snames=name.slice(1,3)
+console.log(snames);
+
+let joinArray=names.join("-----")
+console.log(joinArray);
+
+console.log(name.sort());
